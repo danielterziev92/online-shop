95debb64d03666066a6db01e2858fdcfc393d2c8
Daniel Terziev <daniel.st.terziev@gmail.com>
2024-12-28 01:30:16 +0200

    Add unit tests for AccountAppManager methods
    - Test creation of inactive user with valid credentials
    - Test creation of active user with valid credentials
    - Test creation of superuser with valid credentials
    - Test creation of user with empty email raises ValueError
    - Test normalization of uppercase email during user creation

---
+++ backend/tests/base.py
@@ -0,0 +1,21 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from ecommerce.accounts.models import BaseAccount
+
+
+class AppTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.AccountModel: BaseAccount = get_user_model()
+        cls.account_valid_data = {'email': 'test@email.com', 'password': 'P@ssword1'}
+
+    def setUp(self) -> None:
+        print(f'Test Started: {self._testMethodName}')
+
+    @classmethod
+    def create_account(cls, email: str = 'test@email.com', password: str = 'P@ssowrd1',
+                       is_active: bool = False) -> BaseAccount:
+        model = cls.AccountModel
+        creator = model.objects.create_account_active if is_active else model.objects.create_account_no_active
+        return creator(email=email, password=password)
