fedc8ccb173cffc32a3720a7fc6308977cbe9f6f
Daniel Terziev <daniel.st.terziev@gmail.com>
2024-12-27 20:38:48 +0200

    Implement Ninja framework in project

--- backend/ecommerce/urls.py
+++ backend/ecommerce/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from ninja import NinjaAPI
 
-urlpatterns = [
+api = NinjaAPI()
+
+urlpatterns = (
     path('admin/', admin.site.urls),
-]
+    path('api/', api.urls),
+)
