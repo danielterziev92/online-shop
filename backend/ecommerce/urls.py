680f451ae638651fd40bc5435d0348eb4034ffb3
Daniel Terziev <daniel.st.terziev@gmail.com>
2024-12-28 20:34:44 +0200

    Adding docs to urls in ecommerce/urls.py

--- backend/ecommerce/urls.py
+++ backend/ecommerce/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI
 
-api = NinjaAPI()
+from ecommerce.accounts.views.sign_in import sign_in_router
+
+api = NinjaAPI(docs_url='docs/')
+api.add_router('auth/', router=sign_in_router)
 
 urlpatterns = (
     path('admin/', admin.site.urls),
