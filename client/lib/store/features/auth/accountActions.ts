9e8326df5db00f1d363a03366866f40cff0744f3
Daniel Terziev <daniel.st.terziev@gmail.com>
2024-12-31 23:50:09 +0200

    Key changes: 1. Renamed signIn to signInAction 2. Added verifyCodeAction for code verification 3. Consistent error handling across actions 4. JWT decoding for both signIn and verifyCode
    These updates enhance authentication functionality and maintain consistent error handling.

--- client/lib/store/features/auth/accountActions.ts
+++ client/lib/store/features/auth/accountActions.ts
@@ -15,7 +15,8 @@ interface JWTPayload {
     is_verified: boolean;
 }
 
-export const signIn = createAsyncThunk(
+
+export const signInAction = createAsyncThunk(
     'account/signIn',
     async (credentials: SignInCredentials, {rejectWithValue}) => {
         try {
@@ -59,6 +60,32 @@ export const signUpAction = createAsyncThunk(
             }
 
             return data.message;
+        } catch (error: any) {
+            return rejectWithValue(error.message);
+        }
+    }
+);
+
+export const verifyCodeAction = createAsyncThunk(
+    'account/verifyCode',
+    async (code: string, {rejectWithValue}) => {
+        try {
+            const response = await fetch(API_ROUTES.AUTH.VERIFY_CODE, {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({code}),
+            });
+
+            const data = await response.json();
+
+            if (!response.ok) {
+                throw new Error(data.error || 'Sign in failed');
+            }
+
+            const token = data.token;
+            const decodedToken = jwtDecode<JWTPayload>(token);
+
+            return {accountId: decodedToken.account_id, isVerified: decodedToken.is_verified}
         } catch (error: any) {
             return rejectWithValue(error.message);
         }
